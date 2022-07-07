1cfb8468a58ae6649f4fbf1a136ab50c0c1dc2dd
acleret69 <72598333+acleret69@users.noreply.github.com>
2022-07-07 11:39:45 +0200

    add List and function afficheMenu and ajouterPlat

--- Menu.cs
+++ Menu.cs
@@ -3,8 +3,8 @@ namespace ESGI_C_;
 public class Menu
 {
     private static Menu _instance = null;
-
-     public int nbRepas;
+    
+    public List<Object> plats  { get; set; }
     /*public List<dynamic> burgerViandesList { get; set; }
     public List<dynamic> burgerVegetarienList { get; set; }
     public List<dynamic> pizzaViandesList { get; set; }
@@ -12,6 +12,7 @@ public class Menu
 
     private Menu()
     {
+        plats = new List<Object>();
     }
 
     public static Menu Instance()
@@ -19,5 +20,30 @@ public class Menu
         if (_instance == null)
             _instance = new Menu();
         return _instance;
+    }
+
+    public void AjouterPlat(Object plat)
+    {
+        plats.Add(plat);
+    }
+
+    public void AfficherMenu()
+    {
+        foreach (Object plat in plats)
+        {
+            if (plat.GetType() == typeof(BurgerVegetarien)) 
+            {
+                ((BurgerVegetarien)plat).afficherRecette();
+            }else if (plat.GetType() == typeof(BurgerViandes)) 
+            {
+                ((BurgerViandes)plat).afficherRecette();
+            }else if (plat.GetType() == typeof(PizzaVegetarien)) 
+            {
+                ((PizzaVegetarien)plat).afficherRecette();
+            }else if (plat.GetType() == typeof(PizzaViande)) 
+            {
+                ((PizzaViande)plat).afficherRecette();
+            }
+        }
     }
 }
